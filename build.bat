8e458969c64fa77ad411b23d4ef7f3c62aac3870
bamchoh <bamchoh@gmail.com>
2018-07-26 00:54:44 +0900

    Change build script to Rakefile

--- build.bat
+++ build.bat
@@ -2,52 +2,9 @@
 
 setlocal
 
-doskey ls=ls --color=auto --show-control-chars -N $*
-doskey grep=grep --color=auto $*
-doskey rm=rm -i $*
-doskey cp=cp -i $*
-doskey mv=mv -i $*
-doskey vi=vim $*
-doskey find=c:/msys64/usr/bin/find.exe $*
-doskey ag=d:\apps\ag
-
 set PATH=%PATH%;%PROGRAMFILES%\Git\bin
 set PATH=%PATH%;C:\msys64\mingw64\bin
 set PATH=%PATH%;C:\msys64\usr\bin
-set LUA_VERSION=51
-set LUAPATH=c:/dev/lua%LUA_VERSION%/src
-set PY3_VER=37
-set PY3PATH=%LOCALAPPDATA%/Programs/Python/Python%PY3_VER%-32
-set RB_VER=25
-set RB_API_VER=2.5.0
-set RB_PATH=C:/Ruby%RB_VER%-x64
+set PATH=%PATH%;C:\Ruby25-x64\bin
 
-git clone https://github.com/vim/vim
-
-if "%ERRORLEVEL%" == "128" (
-  echo "Git Pull"
-  cd vim\src
-  git pull
-) else (
-  cd vim\src
-)
-
-REM gvim.exe
-echo Start to build gvim.exe
-make -f Make_ming.mak clean
-make -f Make_ming.mak TERMINAL=yes STATIC_STDCPLUS=yes DEBUG=no LUA=%LUAPATH% DYNAMIC_LUA=yes LUA_VER=%LUA_VERSION% PYTHON3=%PY3PATH% DYNAMIC_PYTHON3=yes PYTHON3_VER=%PY3_VER% RUBY=%RB_PATH% DYNAMIC_RUBY=yes RUBY_VER=%RB_VER% RUBY_API_VER_LONG=%RB_API_VER%
-
-if exist gvim.exe (
-  mv C:/Vim/gvim.exe C:/Vim/gvim_old.exe
-	mv gvim.exe C:/Vim/gvim.exe
-)
-
-REM vim.exe
-echo Start to build vim.exe
-make -f Make_ming.mak clean
-make -f Make_ming.mak GUI=NO TERMINAL=yes STATIC_STDCPLUS=yes DEBUG=no LUA=%LUAPATH% DYNAMIC_LUA=yes LUA_VER=%LUA_VERSION% PYTHON3=%PY3PATH% DYNAMIC_PYTHON3=yes PYTHON3_VER=%PY3_VER% RUBY=%RB_PATH% DYNAMIC_RUBY=yes RUBY_VER=%RB_VER% RUBY_API_VER_LONG=%RB_API_VER%
-
-if exist vim.exe (
-  mv C:/Vim/vim.exe C:/Vim/vim_old.exe
-	mv vim.exe C:/Vim/vim.exe
-)
+rake

1242705dd93b4bb552253efe5db2dd5b84323ac0
bamchoh <bamchoh@gmail.com>
2019-01-20 18:28:16 +0900

    Fix typo

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 setlocal
 
 set PATH=%PATH%;%PROGRAMFILES%\Git\bin
-set PATH=C:\Ruby25-x64\bin:%PATH%
+set PATH=C:\Ruby25-x64\bin;%PATH%
 set PATH=C:\msys64\usr\bin;%PATH%
 set PATH=C:\msys64\mingw64\bin;%PATH%
 
